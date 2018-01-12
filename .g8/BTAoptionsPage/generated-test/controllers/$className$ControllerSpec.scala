73f9c2d4fc48db875e9f3a0863f4b823ececd995
David Wright <davew@MacBook-Pro.local>
2018-01-12 12:53:45 +0000

    BTA specifc optionsPage template created

---
+++ .g8/BTAoptionsPage/generated-test/controllers/$className$ControllerSpec.scala
@@ -0,0 +1,71 @@
+package controllers
+
+import play.api.data.Form
+import play.api.libs.json.JsString
+import uk.gov.hmrc.http.cache.client.CacheMap
+import utils.FakeNavigator
+import connectors.FakeDataCacheConnector
+import controllers.actions.{FakeServiceInfoAction, _}
+import play.api.test.Helpers._
+import forms.$className$FormProvider
+import identifiers.$className$Id
+import models.$className$
+import play.twirl.api.HtmlFormat
+import views.html.$className;format="decap"$
+
+class $className$ControllerSpec extends ControllerSpecBase {
+
+  def onwardRoute = routes.IndexController.onPageLoad()
+
+  val formProvider = new $className$FormProvider()
+  val form = formProvider()
+
+  def controller(dataRetrievalAction: DataRetrievalAction = getEmptyCacheMap) =
+    new $className$Controller(frontendAppConfig, messagesApi, FakeDataCacheConnector, new FakeNavigator(desiredRoute = onwardRoute), FakeAuthAction,
+      FakeServiceInfoAction, formProvider)
+
+  def viewAsString(form: Form[_] = form) = $className;format="decap"$(frontendAppConfig, form)(HtmlFormat.empty)(fakeRequest, messages).toString
+
+  "$className$ Controller" must {
+
+    "return OK and the correct view for a GET" in {
+      val result = controller().onPageLoad()(fakeRequest)
+
+      status(result) mustBe OK
+      contentAsString(result) mustBe viewAsString()
+    }
+
+    "redirect to the next page when valid data is submitted" in {
+      val postRequest = fakeRequest.withFormUrlEncodedBody(("value", $className$.options.head.value))
+
+      val result = controller().onSubmit()(postRequest)
+
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result) mustBe Some(onwardRoute.url)
+    }
+
+    "return a Bad Request and errors when invalid data is submitted" in {
+      val postRequest = fakeRequest.withFormUrlEncodedBody(("value", "invalid value"))
+      val boundForm = form.bind(Map("value" -> "invalid value"))
+
+      val result = controller().onSubmit()(postRequest)
+
+      status(result) mustBe BAD_REQUEST
+      contentAsString(result) mustBe viewAsString(boundForm)
+    }
+
+    "return OK if no existing data is found" in {
+      val result = controller(dontGetAnyData).onPageLoad()(fakeRequest)
+
+      status(result) mustBe OK
+    }
+
+    "redirect to next page when valid data is submitted and no existing data is found" in {
+      val postRequest = fakeRequest.withFormUrlEncodedBody(("value", ($className$.options.head.value)))
+      val result = controller(dontGetAnyData).onSubmit()(postRequest)
+
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result) mustBe Some(onwardRoute.url)
+    }
+  }
+}
