9e86b3ab9d6a5e3c3342529f5ce243d2b9ac4472
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2018-05-02 10:11:43 +0100

    BEACD-8 | added controller template

---
+++ .g8/BTAcontroller/generated-test/controllers/$package$/$className$ControllerSpec.scala
@@ -0,0 +1,29 @@
+package controllers.$package$
+
+import controllers._
+import controllers.actions._
+import play.api.test.Helpers._
+import play.twirl.api.HtmlFormat
+import views.html.$package$.$className;format="decap"$
+
+class $className$ControllerSpec extends ControllerSpecBase {
+
+  def controller(dataRetrievalAction: DataRetrievalAction = getEmptyCacheMap) =
+    new $className$Controller(frontendAppConfig, messagesApi, FakeAuthAction, FakeServiceInfoAction)
+
+  def viewAsString() = ???
+
+  "$className$ Controller" must {
+
+    "return OK and the correct view for a GET" in {
+      val result = controller().onPageLoad(fakeRequest)
+
+      status(result) mustBe OK
+      contentAsString(result) mustBe viewAsString()
+    }
+  }
+}
+
+
+
+
