a00bded7a84771d3ebf68a948b1b80ea726ce843
gaurav-hmrc <gaurav.sharma@digital.hmrc.gov.uk>
2018-05-10 09:52:21 +0000

    BEACD-41 added template with BTA home page link

---
+++ .g8/BTAhomeLinkPage/generated-test/views/$package$/$className$ViewSpec.scala
@@ -0,0 +1,28 @@
+package views.$package$
+
+import play.twirl.api.HtmlFormat
+import views.behaviours.ViewBehaviours
+import views.html.$package$.$className;format="decap"$
+
+class $className$ViewSpec extends ViewBehaviours {
+
+  val messageKeyPrefix = "$className;format="decap"$"
+
+  def createView = () => $className;format="decap"$(frontendAppConfig)(HtmlFormat.empty)(fakeRequest, messages)
+
+  "$className$ view" must {
+    behave like normalPage(createView, messageKeyPrefix)
+
+    "Render the correct content" in {
+      val doc =  asDocument(createView())
+      val view = doc.text()
+
+      assertLinkById(
+        doc,
+        "continue",
+        "$continueButton$",
+        "???",
+        "$gaEventPrefix$:Click:Continue")
+    }
+  }
+}
---
+++ .g8/BTAhomeLinkPage/migrations/$className__snake$.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Applying migration $className;format="snake"$"
+
+echo "Adding routes to conf/app.routes"
+echo "" >> ../conf/app.routes
+echo "GET        $url$                       controllers.$package$.$className$Controller.onPageLoad()" >> ../conf/app.routes
+
+echo "Adding messages to conf.messages (English)"
+echo "" >> ../conf/messages.en
+echo "#######################################################" >> ../conf/messages.en
+echo "##  $className$" >> ../conf/messages.en
+echo "#######################################################" >> ../conf/messages.en
+echo "$className;format="decap"$.title = $pageTitle$" >> ../conf/messages.en
+echo "$className;format="decap"$.heading = $pageHeading$" >> ../conf/messages.en
+echo "$className;format="decap"$.continue = $continueButton$" >> ../conf/messages.en
+echo "$className;format="decap"$.notnow = $btaLink$" >> ../conf/messages.en
+
+echo "Adding messages to conf.messages (Welsh)"
+echo "" >> ../conf/messages.cy
+echo "#######################################################" >> ../conf/messages.cy
+echo "##  $className$" >> ../conf/messages.cy
+echo "#######################################################" >> ../conf/messages.cy
+echo "$className;format="decap"$.title = $welshPageTitle$" >> ../conf/messages.cy
+echo "$className;format="decap"$.heading = $welshPageHeading$" >> ../conf/messages.cy
+echo "$className;format="decap"$.continue = $welshContinueButton$" >> ../conf/messages.cy
+echo "$className;format="decap"$.notnow = $welshBtaLink$" >> ../conf/messages.cy
+
+echo "Moving test files from generated-test/ to test/"
+rsync -avm --include='*.scala' -f 'hide,! */' ../generated-test/ ../test/
+rm -rf ../generated-test/
+
+echo "Migration $className;format="snake"$ completed"
