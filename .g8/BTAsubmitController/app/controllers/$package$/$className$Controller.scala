ea652cbfd54d0fd4e619e367bf05e1d345ca0440
Kurt Logan <kurt.logan@hotmail.co.uk>
2018-05-14 10:21:07 +0100

    Update templates (#62)
    * added a controller with submit action
    
    * imported navigator
    
    * import form
    
    * updated tests
    
    * updated controller template
    
    * Updated controller in template
    
    * added inport
    
    * updated migration
    
    * updated migration
    
    * fixed controller test

---
+++ .g8/BTAsubmitController/app/controllers/$package$/$className$Controller.scala
@@ -0,0 +1,37 @@
+package controllers.$package$
+
+import javax.inject.Inject
+
+import controllers.actions._
+import config.FrontendAppConfig
+import forms.$modelPackage$.$model$FormProvider
+import identifiers.$className$Id
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import utils.Navigator
+
+import scala.concurrent.Future
+
+class $className;format="cap"$Controller @Inject()(appConfig: FrontendAppConfig,
+                                          override val messagesApi: MessagesApi,
+                                          authenticate: AuthAction,
+                                          navigator: Navigator,
+                                          serviceInfo: ServiceInfoAction,
+                                          formProvider: $model$FormProvider) extends FrontendController with I18nSupport {
+
+  val form = formProvider()
+
+  def onPageLoad = (authenticate andThen serviceInfo) {
+    implicit request =>
+      ???
+  }
+
+  def onSubmit = (authenticate andThen serviceInfo).async {
+    implicit request =>
+      form.bindFromRequest().fold(
+        (formWithErrors: Form[_]) => ???,
+        (value) => Future.successful(Redirect(navigator.nextPage($className$Id, value)))
+      )
+  }
+}
