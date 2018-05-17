3cf29e5ab50777b199b548e67e8184a95f024944
Prashant Choudhari <prashant.choudhari@digital.hmrc.gov.uk>
2018-05-17 11:28:26 +0100

    BEACD-18: AEOI

--- test/views/other/aeio/register/RegisterAEOIViewSpec.scala
+++ test/views/other/aeio/register/RegisterAEOIViewSpec.scala
@@ -39,6 +39,10 @@ class RegisterAEOIViewSpec extends ViewBehaviours {
         "Continue - register for AEOI",
         "http://localhost:8080/portal/fatca-registration?lang=eng",
         "RegisterAeoi:Click:Continue")
+
+      assertContainsText(doc, "You need to register for AEOI before you can add it to your account.")
+      assertContainsText(doc, "We’ll send you an Automatic Exchange of Information ID, which you can use to add AEOI to the account.")
+
     }
   }
 }
