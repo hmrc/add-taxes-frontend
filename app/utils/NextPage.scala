385545d5ae9411add33f0e76b25986c5ecca145a
Liam Oldfield <liam.oldfield@digital.hmrc.gov.uk>
2018-07-10 13:12:56 +0100

    BEACD-158 | refactor next page imports

--- app/utils/NextPage.scala
+++ app/utils/NextPage.scala
@@ -19,7 +19,7 @@ package utils
 import config.FrontendAppConfig
 import play.api.mvc.{Call, Request}
 import utils.nextpage.OtherTaxesNextPage
-import utils.nextpage.deenrolment.{DoYouNeedToStopEPAYENextPage, DoYouNeedToStopRONextPage, DoYouWantToLeaveCISNextPage}
+import utils.nextpage.deenrolment._
 import utils.nextpage.employer.cis.IsYourBusinessInUKNextPage
 import utils.nextpage.employer.cis.uk.AreYouContractorOrSubcontractorNextPage
 import utils.nextpage.employer.cis.uk.subcontractor._
