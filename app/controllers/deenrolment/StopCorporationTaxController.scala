ad355e4c5e24dfe4c6d3a130977a486243d9adc4
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2018-07-13 16:21:02 +0100

    BEACD-193 | added missing files

---
+++ app/controllers/deenrolment/StopCorporationTaxController.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.deenrolment
+
+import javax.inject.Inject
+
+import config.FrontendAppConfig
+import controllers.actions._
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import utils.{Enumerable, Navigator}
+
+import forms.deenrolment.StopCorporationTaxFormProvider
+import identifiers.StopCorporationTaxId
+import views.html.deenrolment.stopCorporationTax
+
+import scala.concurrent.Future
+
+class StopCorporationTaxController @Inject()(
+  appConfig: FrontendAppConfig,
+  override val messagesApi: MessagesApi,
+  navigator: Navigator,
+  authenticate: AuthAction,
+  serviceInfoData: ServiceInfoAction,
+  formProvider: StopCorporationTaxFormProvider)
+    extends FrontendController
+    with I18nSupport
+    with Enumerable.Implicits {
+
+  val form = formProvider()
+
+  def onPageLoad() = (authenticate andThen serviceInfoData) { implicit request =>
+    Ok(stopCorporationTax(appConfig, form)(request.serviceInfoContent))
+  }
+
+  def onSubmit() = (authenticate andThen serviceInfoData) { implicit request =>
+    form
+      .bindFromRequest()
+      .fold(
+        (formWithErrors: Form[_]) =>
+          BadRequest(stopCorporationTax(appConfig, formWithErrors)(request.serviceInfoContent)),
+        (value) => Redirect(navigator.nextPage(StopCorporationTaxId, value))
+      )
+  }
+}
---
+++ app/utils/nextpage/deenrolment/StopCorporationTaxNextPage.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils.nextpage.deenrolment
+
+import config.FrontendAppConfig
+import identifiers.StopCorporationTaxId
+import models.deenrolment.StopCorporationTax
+import play.api.mvc.{Call, Request}
+import utils.Enrolments.CT
+import utils.NextPage
+
+trait StopCorporationTaxNextPage {
+
+  implicit val stopCorporationTax: NextPage[StopCorporationTaxId.type, StopCorporationTax] = {
+    new NextPage[StopCorporationTaxId.type, StopCorporationTax] {
+      override def get(b: StopCorporationTax)(implicit appConfig: FrontendAppConfig, request: Request[_]): Call =
+        b match {
+          case StopCorporationTax.Stop    => Call("GET", appConfig.emacDeenrolmentsUrl(CT))
+          case StopCorporationTax.Dormant => Call("GET", appConfig.getGovUKUrl("dormant-ct"))
+          case StopCorporationTax.Close   => Call("GET", appConfig.getGovUKUrl("closing-company"))
+        }
+    }
+  }
+}
