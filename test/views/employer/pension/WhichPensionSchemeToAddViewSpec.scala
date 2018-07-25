597fe4815b3c04ca6a9494ac4cbfd5b88114b36d
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2018-07-25 15:21:41 +0100

    Added suffix to browser title

--- test/views/employer/pension/WhichPensionSchemeToAddViewSpec.scala
+++ test/views/employer/pension/WhichPensionSchemeToAddViewSpec.scala
@@ -73,8 +73,9 @@ class WhichPensionSchemeToAddViewSpec extends ViewBehaviours {
     "invalid data is sent" must {
       "prepend title with Error: " in {
         val doc = asDocument(createViewUsingForm(form.bind(Map("value" -> ""))))
+        val title = messages("site.service_title", messages(s"$messageKeyPrefix.title"))
 
-        assertEqualsMessage(doc, "title", "error.browser.title", messages(s"$messageKeyPrefix.title"))
+        assertEqualsMessage(doc, "title", "error.browser.title", title)
       }
     }
   }
