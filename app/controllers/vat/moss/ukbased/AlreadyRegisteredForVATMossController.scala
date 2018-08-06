b9f3dc37f499bd7a16daab4fc1d539603459e27d
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2018-08-06 14:33:37 +0100

    BEACD-218 | Added vat moss page for uk based

---
+++ app/controllers/vat/moss/ukbased/AlreadyRegisteredForVATMossController.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.vat.moss.ukbased
+
+import config.FrontendAppConfig
+import controllers.actions._
+import forms.vat.moss.AlreadyRegisteredForVATMossFormProvider
+import identifiers.AlreadyRegisteredForVATMossId
+import javax.inject.Inject
+import models.requests.ServiceInfoRequest
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{AnyContent, Call}
+import play.twirl.api.Html
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import utils.Navigator
+import viewmodels.ViewAction
+import views.html.vat.moss.alreadyRegisteredForVATMoss
+
+class AlreadyRegisteredForVATMossController @Inject()(
+  appConfig: FrontendAppConfig,
+  override val messagesApi: MessagesApi,
+  authenticate: AuthAction,
+  navigator: Navigator[Call],
+  serviceInfo: ServiceInfoAction,
+  formProvider: AlreadyRegisteredForVATMossFormProvider)
+    extends FrontendController
+    with I18nSupport {
+
+  val form = formProvider()
+  val viewAction = ViewAction(routes.AlreadyRegisteredForVATMossController.onSubmit(), "VatMossNoVatUkVatRegistered")
+
+  def view(form: Form[_] = form)(implicit request: ServiceInfoRequest[_]): Html =
+    alreadyRegisteredForVATMoss(appConfig, form, viewAction)(request.serviceInfoContent)
+
+  def onPageLoad = (authenticate andThen serviceInfo) { implicit request =>
+    Ok(view())
+  }
+
+  def onSubmit = (authenticate andThen serviceInfo) { implicit request =>
+    form
+      .bindFromRequest()
+      .fold(
+        (formWithErrors: Form[_]) => BadRequest(view(formWithErrors)),
+        (value) => Redirect(navigator.nextPage(AlreadyRegisteredForVATMossId, value))
+      )
+  }
+}
--- test/controllers/vat/moss/ukbased/AlreadyRegisteredForVATMossControllerSpec.scala
+++ test/controllers/vat/moss/ukbased/AlreadyRegisteredForVATMossControllerSpec.scala
@@ -14,12 +14,11 @@
  * limitations under the License.
  */
 
-package controllers.vat.moss.iom
+package controllers.vat.moss.ukbased
 
 import controllers._
-import controllers.actions.{FakeServiceInfoAction, _}
+import controllers.actions._
 import forms.vat.moss.AlreadyRegisteredForVATMossFormProvider
-import models.vat.moss.AlreadyRegisteredForVATMoss
 import play.api.data.Form
 import play.api.mvc.Call
 import play.api.test.Helpers._
@@ -34,14 +33,15 @@ class AlreadyRegisteredForVATMossControllerSpec extends ControllerSpecBase {
 
   val formProvider = new AlreadyRegisteredForVATMossFormProvider()
   val form = formProvider()
-  val viewAction = ViewAction(routes.AlreadyRegisteredForVATMossController.onSubmit(), "VatMossNoVatIomVatRegistered")
+  lazy val viewAction =
+    ViewAction(routes.AlreadyRegisteredForVATMossController.onSubmit(), "VatMossNoVatUkVatRegistered")
 
-  def controller() =
+  def controller(dataRetrievalAction: DataRetrievalAction = getEmptyCacheMap) =
     new AlreadyRegisteredForVATMossController(
       frontendAppConfig,
       messagesApi,
-      new FakeNavigator[Call](desiredRoute = onwardRoute),
       FakeAuthAction,
+      new FakeNavigator[Call](desiredRoute = onwardRoute),
       FakeServiceInfoAction,
       formProvider)
 
@@ -51,45 +51,28 @@ class AlreadyRegisteredForVATMossControllerSpec extends ControllerSpecBase {
   "AlreadyRegisteredForVATMoss Controller" must {
 
     "return OK and the correct view for a GET" in {
-      val result = controller().onPageLoad()(fakeRequest)
+      val result = controller().onPageLoad(fakeRequest)
 
       status(result) mustBe OK
       contentAsString(result) mustBe viewAsString()
     }
 
     "redirect to the next page when valid data is submitted" in {
-      val postRequest = fakeRequest.withFormUrlEncodedBody(("value", AlreadyRegisteredForVATMoss.options.head.value))
-
-      val result = controller().onSubmit()(postRequest)
+      val postRequest = fakeRequest.withFormUrlEncodedBody("value" -> "Yes")
+      val result = controller().onSubmit(postRequest)
 
       status(result) mustBe SEE_OTHER
       redirectLocation(result) mustBe Some(onwardRoute.url)
     }
 
-    "return a Bad Request and errors when invalid data is submitted" in {
-      val postRequest = fakeRequest.withFormUrlEncodedBody(("value", "invalid value"))
-      val boundForm = form.bind(Map("value" -> "invalid value"))
+    "return BadRequest when invalid data is submitted" in {
+      val postRequest = fakeRequest.withFormUrlEncodedBody("value" -> "")
+      val boundForm = form.bind(Map("value" -> ""))
 
-      val result = controller().onSubmit()(postRequest)
+      val result = controller().onSubmit(postRequest)
 
       status(result) mustBe BAD_REQUEST
       contentAsString(result) mustBe viewAsString(boundForm)
-    }
-
-    "return OK" in {
-      val result = controller().onPageLoad()(fakeRequest)
-
-      status(result) mustBe OK
-    }
-
-    for (option <- AlreadyRegisteredForVATMoss.options) {
-      s"redirect to next page when '${option.value}' is submitted" in {
-        val postRequest = fakeRequest.withFormUrlEncodedBody(("value", (option.value)))
-        val result = controller().onSubmit()(postRequest)
-
-        status(result) mustBe SEE_OTHER
-        redirectLocation(result) mustBe Some(onwardRoute.url)
-      }
     }
   }
 }
