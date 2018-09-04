a82e198f33b3f23b2a5a37ec7d7f9305e3935603
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2018-09-04 11:53:09 +0100

    BEACD-253 | added content

--- app/controllers/employer/PartnershipRegisterByPhoneController.scala
+++ app/controllers/employer/PartnershipRegisterByPhoneController.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers.employer
 
 import javax.inject.Inject
@@ -10,13 +26,15 @@ import views.html.employer.partnershipRegisterByPhone
 
 import scala.concurrent.Future
 
-class PartnershipRegisterByPhoneController @Inject()(appConfig: FrontendAppConfig,
-                                          override val messagesApi: MessagesApi,
-                                          authenticate: AuthAction,
-                                          serviceInfo: ServiceInfoAction ) extends FrontendController with I18nSupport {
+class PartnershipRegisterByPhoneController @Inject()(
+  appConfig: FrontendAppConfig,
+  override val messagesApi: MessagesApi,
+  authenticate: AuthAction,
+  serviceInfo: ServiceInfoAction)
+    extends FrontendController
+    with I18nSupport {
 
-  def onPageLoad = (authenticate andThen serviceInfo) {
-    implicit request =>
-      Ok(partnershipRegisterByPhone(appConfig)(request.serviceInfoContent))
+  def onPageLoad = (authenticate andThen serviceInfo) { implicit request =>
+    Ok(partnershipRegisterByPhone(appConfig)(request.serviceInfoContent))
   }
 }
