4d8cc39878010a72cd06408fe2f855e98529e13a
“nehakannaujia1” <neha.kannaujia1@digital.hmrc.gov.uk>
2020-02-05 15:00:54 +0000

    BTA-1863: Added focus to error summery when there is any error

---
+++ app/assets/javascripts/error_summary_focus.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    // If there is an error summary, set focus to the summary
+    if ($('.error-summary--show').length) {
+      $('.error-summary--show').focus()
+    }
+})
