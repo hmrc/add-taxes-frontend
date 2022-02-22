fc8df57262610df7e6e8bd22cd24f45e98abedb6
rajanimohan22 <mohan.dolla@digital.hmrc.gov.uk>
2022-02-22 10:02:41 +0000

    DL-7142 Update Dependencies and Remove all Warns from add-taxes-fronted

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,10 +7,11 @@ private object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.16.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.20.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % "0.59.0",
     "uk.gov.hmrc" %% "http-caching-client" % "9.5.0-play-28",
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "2.0.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "3.4.0-play-28",
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala" %  "2.13.1"
   )
 
   abstract class TestDependencies(scope: String) {
@@ -18,12 +19,12 @@ private object AppDependencies {
       "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % scope,
       "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
       "org.pegdown" % "pegdown" % "1.6.0" % scope,
-      "org.jsoup" % "jsoup" % "1.13.1" % scope,
-      "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.14.0" % scope,
+      "org.jsoup" % "jsoup" % "1.14.3" % scope,
+      "uk.gov.hmrc" %% "bootstrap-test-play-28" % "5.20.0" % scope,
       "org.scalacheck" %% "scalacheck" % "1.15.4" % scope,
       "org.scalatestplus" %% "mockito-3-12" % "3.2.10.0" % scope,
-      "com.github.tomakehurst" % "wiremock-jre8" % "2.27.2" % scope,
-      "org.mockito" % "mockito-core" % "3.7.7" % scope
+      "com.github.tomakehurst" % "wiremock-jre8" % "2.32.0" % scope,
+      "org.mockito" % "mockito-core" % "4.3.1" % scope
     )
   }
 
