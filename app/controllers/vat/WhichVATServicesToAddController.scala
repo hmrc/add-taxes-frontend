2682d0941698b70340f10141237df5f0f14449c7
bwardak <157559558+bwardak@users.noreply.github.com>
2026-02-18 14:54:39 +0000

    DL-17715: Fix error summary link. Fix vat button appearing even when enrolled with mtd vat enrolment

--- app/controllers/vat/WhichVATServicesToAddController.scala
+++ app/controllers/vat/WhichVATServicesToAddController.scala
@@ -1,4 +1,20 @@
 /*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * Copyright 2024 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -54,7 +70,7 @@ class WhichVATServicesToAddController @Inject()(mcc: MessagesControllerComponent
     val enrolments = request.request.enrolments
     WhichVATServicesToAdd.options
       .filterNot(x =>
-        if (enrolments.getEnrolment("HMRC_MTD_VAT").isDefined || enrolments.getEnrolment("HMCE-VATDEC-ORG").isDefined) {
+        if (checkForVatEnrolment) {
           x.value == WhichVATServicesToAdd.VAT.toString
         } else {
           false
@@ -66,17 +82,25 @@ class WhichVATServicesToAddController @Inject()(mcc: MessagesControllerComponent
         false
       }
     )
+  }
 
+  private def checkForVatEnrolment(implicit request: ServiceInfoRequest[AnyContent]): Boolean = {
+    val enrolments = request.request.enrolments
+    if (enrolments.getEnrolment("HMRC-MTD-VAT").isDefined || enrolments.getEnrolment("HMCE-VATDEC-ORG").isDefined) {
+      true
+    } else {
+      false
+    }
   }
 
   def onPageLoad(): Action[AnyContent] = (authenticate andThen serviceInfoData) { implicit request =>
-    Ok(whichVATServicesToAdd(appConfig, form, radioOptions)(request.serviceInfoContent))
+    Ok(whichVATServicesToAdd(appConfig, form, radioOptions, checkForVatEnrolment)(request.serviceInfoContent))
   }
 
   def onSubmit(): Action[AnyContent] = (authenticate andThen serviceInfoData).async { implicit request =>
     form.bindFromRequest()
       .fold(
-        formWithErrors => Future.successful(BadRequest(whichVATServicesToAdd(appConfig, formWithErrors, radioOptions)(request.serviceInfoContent))),
+        formWithErrors => Future.successful(BadRequest(whichVATServicesToAdd(appConfig, formWithErrors, radioOptions, checkForVatEnrolment)(request.serviceInfoContent))),
         value =>
           value match {
             case _ if(value == WhichVATServicesToAdd.VATIOSS || value == WhichVATServicesToAdd.VATOSS) =>
