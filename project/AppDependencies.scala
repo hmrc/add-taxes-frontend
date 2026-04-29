2a537d20908d56b04fb0cec9a9d5146402ab4953
kannanekambaram <112036677+kannanEkambaram@users.noreply.github.com>
2026-04-29 12:53:06 +0100

    DL-18771: Update the library for service nav changes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ private object AppDependencies {
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30"     % bootstrapVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"             % "2.6.0",
     "uk.gov.hmrc"                   %% "http-caching-client-play-30"    % "11.2.0",
-    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"     % "12.10.0",
+    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"     % "12.31.0",
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"           % "2.16.1",
     "uk.gov.hmrc"                   %% "play-conditional-form-mapping-play-30" % "3.3.0"
   )
